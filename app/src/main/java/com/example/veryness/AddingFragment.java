2e05b0d03e4787462c57a0e3a7306379572d8cef
Grandman2003 <goykhman200316@gmail.com>
2020-03-06 19:20:43 +0300

    Urra

---
+++ app/src/main/java/com/example/veryness/AddingFragment.java
@@ -0,0 +1,24 @@
+package com.example.veryness;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AddingFragment extends Fragment {
+    Button button;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        assert container != null;
+        button=container.findViewById(R.id.button);
+
+        return inflater.inflate(R.layout.funfragment, container, false);
+
+    }
+}
---
+++ app/src/main/java/com/example/veryness/RecordingFragment.java
@@ -0,0 +1,23 @@
+package com.example.veryness;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class RecordingFragment extends Fragment {
+   Button button;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        assert container != null;
+        button=container.findViewById(R.id.button);
+
+        return inflater.inflate(R.layout.recfragment, container, false);
+    }
+}
